353a89a6adba9e9a73b53e9d09a1ce9d3fc3be6a
marcomws <marco.mws@outlook.com>
2021-08-07 22:48:57 +0200

    ModalModule

--- src/app/standard-filter/standard-filter.module.ts
+++ src/app/standard-filter/standard-filter.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { StandardFilterComponent } from './standard-filter.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
-  imports: [],
+  imports: [ModalModule.forRoot()],
   declarations: [StandardFilterComponent],
   exports: [StandardFilterComponent],
   bootstrap: [StandardFilterComponent]
